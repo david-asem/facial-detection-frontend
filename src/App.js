26d8f7ff4b7b0af83f1983e97cb20719558500fb
david-asem <ddy.asem@gmail.com>
2022-05-31 10:13:31 +0000

    initial commit of facial detection app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Particle from './components/Particles/Particle';
+import ImageUrlForm from './components/ImageUrlForm/ImageUrlForm';
+import Position from './components/Position/Position';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Particle />
+
+      <Navigation />
+      <Logo />
+      <Position />
+      <ImageUrlForm />
+      {/*
+      
+  <FaceRecognition />*/}
     </div>
   );
 }
---
+++ src/components/Position/Position.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Position = () => {
+  return (
+    <div>
+      <div className="white f3">
+        {"David, your current position is..."}
+      </div>
+      <div className="white f1">
+        {'#1'}
+        </div>
+    </div>
+      
+  );
+}
+
+export default Position;
